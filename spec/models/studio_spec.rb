96d4b711d261a2d5fcc551cf137395d093ebaf9a
shaaibu <shaaibusuleiman@gamil.com>
2023-06-24 13:02:22 +0100

    implement tests for studio model

--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -9,4 +9,51 @@ RSpec.describe Studio, type: :model do
     it 'should create a new studio for user' do
         expect(@studio).to be_valid
     end
+
+    it 'should not create a studio if name is nil' do
+        @studio.name = nil
+        expect(@studio).to_not be_valid
+    end
+
+    it 'should not craete a studio if price is nil' do
+        @studio.price = nil
+        expect(@studio).to_not be_valid
+    end
+
+    it 'should not create a studio if duration is nil' do
+        @studio.duration = nil
+        expect(@studio).to_not be_valid
+    end
+
+    it 'should not create a studio if rating is nil' do
+        @studio.rating = nil
+        expect(@studio).to_not be_valid
+    end
+
+    it 'should not create a studio if price is less than or equal to zero' do
+        @studio.price = -1
+        expect(@studio).to_not be_valid
+    end
+
+    it 'should not create studio if duration is a string' do
+        @studio.duration = 'two hours'
+        expect(@studio).to_not be_valid
+    end
+
+    it 'should not create studio if rating is not in the range of 1 to 5' do
+        @studio.rating = 6
+        expect(@studio).to_not be_valid
+    end
+
+    it 'should not create a studio if description is nil' do
+        @studio.description = nil
+        expect(@studio).to_not be_valid
+    end
+
+    it 'should not create a studio if working is nil' do
+        @studio.working_hours = nil
+        expect(@studio).to_not be_valid
+    end
+
+
 end
