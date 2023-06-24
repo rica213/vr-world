c5bcf7a6684aeee4f4376310bab209943067a1a9
rica213 <25f5k252l@mozmail.com>
2023-06-24 19:25:52 +0300

    Write test about validation in the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe User, type: :model do
       expect(User.reflect_on_association(:reservations).macro).to eq(:has_many)
     end
   end
+
+  context 'Validation' do
+    it 'validates the presence of a username' do
+      expect(User.new(username: nil).valid?).to be false
+    end
+
+    it 'validates the uniqueness of a username' do
+      user_one = User.new(username: 'Tao', email: 'zuigbi@ro.lk', password: '123456789')
+      user_one.save
+      user_two = User.new(username: 'Tao', email:'iko@uman.lt', password: '123456789')
+      expect(user_two.valid?).to be false
+    end
+  end
 end
