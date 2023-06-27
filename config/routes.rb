ed6454e10045439f20d00bdc312fe6127755eb42
Frempong <samankwaa.frempong@gmail.com>
2023-06-27 07:06:24 +0000

    Add /api-docs as root

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
       end
     end
   end
-
+  root to: redirect('/api-docs')
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
