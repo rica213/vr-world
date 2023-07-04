1e2b04fbaf34289f97153bf1056c91128de24829
shaaibu <shaaibusuleiman@gamil.com>
2023-07-04 10:25:06 +0100

    update user permission

--- db/migrate/20230622091211_create_users.rb
+++ db/migrate/20230622091211_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :username
-      t.boolean :admin, default: false
+      t.boolean :admin, default: true
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-userone = User.create(username: "clarrielle", admin: true, email: "clarielle@gmail.com", encrypted_password: "12345678" )
+userone = User.create(username: "clarrielle", admin: true, email: "clarielle@gmail.com", encrypted_password: "12345678")
